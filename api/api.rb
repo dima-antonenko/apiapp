e6b6078519a83832bcfc7cfa5a8892df5a8a6acb
yaros <yaroskrinitsky@gmail.com>
2015-11-14 23:08:17 +0200

    modified according to suggestions

--- api/api.rb
+++ api/api.rb
@@ -5,52 +5,60 @@ require "json"
 class Api < Grape::API
   format :json
 
+  helpers do
+    def connection
+      @conn ||= PG.connect(dbname: 'apiapp')
+    end
+
+    def read_all
+      connection.exec("SELECT * FROM tasks ORDER BY requests_count DESC").to_a
+    end
+
+    def create(attrs)
+      connection.exec(
+        "INSERT INTO tasks VALUES (DEFAULT, '#{attrs[:title]}', '#{attrs[:description]}', 0) RETURNING id, title, description")[0]
+    end
+
+    def read(id)
+      res = connection.exec("SELECT * FROM tasks WHERE id=#{id}")
+      if(res.ntuples > 0) 
+        res[0]
+      else 
+        {}
+      end
+    end
+
+    def update(attrs)
+      connection.exec("UPDATE tasks SET title='#{attrs[:title]}', description='#{attrs[:description]}' WHERE id=#{attrs[:id]} RETURNING id, title, description")[0]
+    end
+
+    def delete(id)
+      connection.exec("DELETE FROM tasks WHERE id=#{id}")
+      id
+    end
+  end
+
   get "/tasks" do
-    conn = PG.connect(:dbname => 'apiapp')
-    queryResult = conn.exec("SELECT * FROM tasks ORDER BY requests_count DESC")
-    resArray = Array.new
-    queryResult.each do |row|
-    	resArray.push({
-    		id: row['id'], 
-    		title: row['title'], 
-    		description: row['description'], 
-    		requests_count: row['requests_count']})
-    end  
-    response = (resArray.map { |o| Hash[o.each_pair.to_a] }).to_json
-    JSON.parse(response)
+    read_all
   end
 
   post "/tasks" do
-  	title = params[:title]
-  	description = params[:description]
-  	conn = PG.connect(:dbname => 'apiapp')
-  	conn.exec("INSERT INTO tasks VALUES (DEFAULT, '#{title}', '#{description}', 0)")
-    {}
+  	attrs = {title: params[:title], description: params[:description]}
+  	create(attrs)
   end
 
   get "/tasks/:id" do
     id = params[:id]
-    conn = PG.connect(:dbname => 'apiapp')
-    queryResult = conn.exec("SELECT * FROM tasks WHERE id=#{id}")
-    if queryResult.ntuples > 0
-      return JSON.parse(queryResult[0].to_json)
-    end
-    {}
+    read(id)
   end
 
   patch "/tasks/:id" do
-    id = params[:id]
-    title = params[:title]
-    description = params[:description]
-    conn = PG.connect(:dbname => 'apiapp')
-    conn.exec("UPDATE tasks SET title='#{title}', description='#{description}' WHERE id=#{id}")
-    {}  
+    attrs = {id: params[:id], title: params[:title], description: params[:description]}
+    update(attrs)
   end
 
   delete "/tasks/:id" do
     id = params[:id]
-    conn = PG.connect(:dbname => 'apiapp')
-    conn.exec("DELETE FROM tasks WHERE id=#{id}")
-    {}
+    delete(id)
   end
 end
